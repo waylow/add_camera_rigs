4e10906b2c84ee169350119027aa9d23a818f71f
Wayne Dixon <thewaylow@gmail.com>
2023-09-23 21:06:27 +1000

    updated for bone collections (working)

--- __init__.py
+++ __init__.py
@@ -1,27 +1,13 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
+# SPDX-FileCopyrightText: 2019-2022 Blender Foundation
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 bl_info = {
     "name": "Add Camera Rigs",
     "author": "Wayne Dixon, Brian Raschko, Kris Wittig, Damien Picard, Flavio Perez",
-    "version": (1, 4, 4),
-    "blender": (2, 80, 0),
-    "location": "View3D > Add > Camera > Dolly or Crane Rig",
+    "version": (1, 5, 1),
+    "blender": (4, 0, 0),
+    "location": "View3D > Add > Camera",
     "description": "Adds a Camera Rig with UI",
     "doc_url": "{BLENDER_MANUAL_URL}/addons/camera/camera_rigs.html",
     "tracker_url": "https://github.com/waylow/add_camera_rigs/issues",
